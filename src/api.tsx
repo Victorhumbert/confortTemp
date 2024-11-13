bb437c94fbb794898ef70a9a22a45af0fad57fe8
Victor Humberto <tormentavictor@gmail.com>
2024-11-12 22:40:36 -0300

    Add function para pegar local e temperatura

--- src/api.tsx
+++ src/api.tsx
@@ -2,7 +2,8 @@ import {ClimaProps, FormProps} from "@/interfaces.tsx";
 
 
 const URL_BASE = "http://192.168.3.47:3000"
-const CLIMA_API_KEY = import.meta.env.VITE_CLIMA_API_KEY;
+// const CLIMA_API_KEY = import.meta.env.VITE_CLIMA_API_KEY;
+const CLIMA_API_KEY = process.env.CLIMA_API_KEY;
 
 export async function enviarDados(dados: FormProps): Promise<void> {
     const response = await fetch(`${URL_BASE}/api/dados`, {
